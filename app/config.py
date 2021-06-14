e53be60d48a4679bffcf77168f3c9056d0bb2df6
Alan Quaino <alanquaino@gmail.com>
2021-06-14 11:37:01 +0200

    Changed config file logic.

--- app/config.py
+++ app/config.py
@@ -5,7 +5,14 @@ basedir = path.abspath(path.dirname(__file__))
 load_dotenv(path.join(basedir, ".env"))
 
 FLASK_APP = "app"
-FLASK_ENV = environ.get("FLASK_ENV")
 SECRET_KEY = environ.get("SECRET_KEY")
 SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+if environ.get("MODE") == "development":
+    FLASK_ENV = "development"
+elif environ.get("MODE") == "production":
+    FLASK_ENV = "production"
+elif environ.get("MODE") == "heroku":
+    FLASK_ENV = "production"
+    SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL").replace("postgres://", "postgresql://", 1)
