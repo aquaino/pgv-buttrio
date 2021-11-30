058de424c27b0fad812f2a019a2d79095c2f5ed5
Alan Quaino <alanquaino@gmail.com>
2021-11-30 22:46:32 +0100

    Added other reports.

--- app/reports/views.py
+++ app/reports/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 from app.auth.views import login_required
-from app.models import db, User, ActivityRecord, UserSubtypeAssociation, UserSubtype, UserType
+from app.models import User, UserSubtypeAssociation, UserSubtype, UserType, Event, ActivityRecord
 from sqlalchemy import create_engine
 from flask import current_app
 from sqlalchemy import func, desc
@@ -22,10 +22,12 @@ def index():
 
     # Number of users
     users_count = User.query.count()
+
     # Amount of hours in different activities
     ah_query = "SELECT end_time - start_time FROM activity_records"
     result = connection.execute(ah_query).fetchall()
     activity_hours = sum([item[0].seconds/3600 for item in result])
+
     # Users by type
     users_by_type = User.query\
         .with_entities(UserType.name, func.count(User.id))\
@@ -36,4 +38,10 @@ def index():
         .order_by(desc(func.count(User.id)))\
         .all()
 
-    return render_template("reports/index.html", users_count=users_count, activity_hours=activity_hours, users_by_type=users_by_type)
+    # Events count
+    events_count = Event.query.count()
+
+    # Days count
+    days_count = ActivityRecord.query.distinct(ActivityRecord.date).count()
+
+    return render_template("reports/index.html", users_count=users_count, activity_hours=activity_hours, users_by_type=users_by_type, events_count=events_count, days_count=days_count)
